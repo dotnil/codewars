551c1f044ca0257bc64324a61edb88adcaa2c62c
Tatiana Kh <ts.khol@gmail.com>
2023-05-06 11:27:20 +0300

    js: add solve of 'Counting sheep' and 'Sum of positive'

--- js/count-of-positives-sum-of-negatives.js
+++ js/count-of-positives-sum-of-negatives.js
@@ -1,12 +1,3 @@
-function countPositivesSumNegatives(input) {
-  if ( input === null || input.length === 0 ) return []
-
-  return input.reduce((acc, num) => {
-    num > 0 ? acc[0]++ : acc[1] += num
-    return acc
-  }, [0, 0])
-}
-
 /*
 Given an array of integers.
 
@@ -17,3 +8,12 @@ If the input is an empty array or is null, return an empty array.
 Example
 For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 */
+
+export function countPositivesSumNegatives(input) {
+  if (input === null || input.length === 0) return []
+
+  return input.reduce((acc, num) => {
+    num > 0 ? acc[0]++ : acc[1] += num
+    return acc
+  }, [0, 0])
+}
