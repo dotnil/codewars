69de9d80869143245185d84f23fbc89690c4b2c3
Tatiana Kh <ts.khol@gmail.com>
2023-06-02 18:54:19 +0300

    add 2 solutions (8 kyu)

---
+++ js/spuare-n-sum.js
@@ -0,0 +1,13 @@
+/* 8 kyu
+Square(n) Sum
+
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 12+22+22=91^2 + 2^2 + 2^2 = 912+22+22=9.
+*/
+
+export function squareSum(numbers) {
+  return numbers.reduce(function(sum, number){
+    return Math.pow(number, 2) + sum
+  }, 0)
+}
