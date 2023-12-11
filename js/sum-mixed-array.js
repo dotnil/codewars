48b414205836aea9f272686a98a55848185e8c1d
Tatiana Kh <ts.khol@gmail.com>
2023-12-11 23:45:14 +0300

    add 3 solutions (8 kyu)

---
+++ js/sum-mixed-array.js
@@ -0,0 +1,10 @@
+/* Sum Mixed Array
+
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+*/
+
+export const sumMix = nums => {
+  return nums.reduce((total, amount) => total + Number(amount), 0)
+}
---
+++ js/total-amount-of-points.js
@@ -0,0 +1,34 @@
+/* Total amount of points
+
+Our football team has finished the championship.
+
+Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format 'x:y', where x is our team's score and y is our opponents score.
+
+For example: ['3:1', '2:2', '0:1', ...]
+
+Points are awarded for each match as follows:
+
+    if x > y: 3 points (win)
+    if x < y: 0 points (loss)
+    if x = y: 1 point (tie)
+
+We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+Notes:
+
+    our team always plays 10 matches in the championship
+    0 <= x <= 4
+    0 <= y <= 4
+*/
+
+export const points = games => {
+  return games.reduce((acc, score) => {
+    const win = 3
+    const loss = 0
+    const tie = 1
+
+    if (score[0] > score[2]) return acc + win
+    if (score[0] < score[2]) return acc + loss
+    return acc + tie
+  }, 0)
+}
